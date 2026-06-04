5bd2f0d3fa50739b39098e771a69b19b16cb7073
pranjal28-stack <singhpranjalvardhan@gmail.com>
2026-06-04 21:02:55 +0530

    Create 09_Performance_Analysis.sql

---
+++ scripts/Analysis/09_Performance_Analysis.sql
@@ -0,0 +1,31 @@
+/* analyze the yearly performance of products by comparing sales of products to both the avg sales performance of the product
+ and the prev year sales*/
+with yearly_product_sales as (
+select 
+year(f.order_date) as order_year,
+p.product_name,
+sum(f.sales_amount) as current_sales
+from gold_fact_sales f 
+left join gold_dim_products p
+on f.product_key = p.product_key
+where f.order_date is not null
+group by year(f.order_date), p.product_name)
+
+select
+order_year,
+product_name,
+current_sales,
+avg(current_sales) over(partition by product_name) avg_sales,
+current_sales- avg(current_sales) over(partition by product_name)  as diff_avg,
+case when current_sales- avg(current_sales) over(partition by product_name)>0  then 'Above Avg'
+     when current_sales- avg(current_sales) over(partition by product_name)<0  then 'Below Avg'
+     else 'Avg'
+end avg_change,
+lag(current_sales) over( partition by product_name order by order_year) as py_sales,
+current_sales - lag(current_sales) over( partition by product_name order by order_year) as diff_py,
+case when current_sales- lag(current_sales) over( partition by product_name order by order_year)>0  then 'Increase'
+     when current_sales- lag(current_sales) over( partition by product_name order by order_year)<0  then 'Decrease'
+     else 'No Change'
+end py_change
+from yearly_product_sales
+order by product_name, order_year;
